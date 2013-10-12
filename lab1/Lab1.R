19d1eb3eb4a89caa07ec805c9a4d821f1ae17a80
Geraud <geraudster@gmail.com>
2013-10-12 13:04:14 +0200

    fin lab1

---
+++ lab1/Lab1.R
@@ -0,0 +1,34 @@
+# Q1
+install.packages("car")
+
+# Q2
+library(car)
+
+# Q3
+search()
+
+# Q4
+?data.frame
+
+# Q5
+v1 = c(1:10)
+v2 = c("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten")
+nw = data.frame(numbers=v1, words=v2)
+attributes(nw)
+
+# Q6
+nw[8,'numbers']
+
+# Q7
+nw[8,'words']
+
+# Q8
+a=matrix(1:10,2,5)
+
+# Q9
+x=c(1:6)
+y=c(5:10)
+rbind(x,y)
+
+# Q10
+cbind(x,y)
